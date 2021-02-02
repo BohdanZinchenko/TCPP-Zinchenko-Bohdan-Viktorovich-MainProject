2753293ef69fe19d6e35b9cf552cd8af9b0f8b7e
Bohdan <zin4enko.bod@gmail.com>
2021-02-02 12:27:14 +0200

    Add new Program With ThreadWork

---
+++ ThreadWork/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace ThreadWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var time = new Stopwatch();
+            time.Start();
+
+            var listOfNum = WorkWithFile.ReadFile();
+            if (listOfNum == null)
+            {
+                return;
+            }
+            var threadWork = new ThreadSafeHashSet();
+
+            threadWork.CreateThread(listOfNum);
+            time.Stop();
+
+            WorkWithFile.CreateResult(time, threadWork.GetList());
+
+
+        }
+
+
+    }
+}
---
+++ ThreadWork/WorkWithFile.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace ThreadWork
+{
+    public static class WorkWithFile
+    {
+        public static List<Settings> ReadFile()
+        {
+            string jsonFile;
+            try
+            {
+                jsonFile = File.ReadAllText("settings.json");
+            }
+            catch
+            {
+                var result = new Result()
+                {
+                    Error = "Not founded file settings.json",
+                    Primes = null,
+                    Success = false,
+                    Duration = null
+                };
+                LoadResult(result);
+                return null;
+            }
+            try
+            {
+                JsonSerializer.Deserialize<List<Settings>>(jsonFile);
+
+            }
+            catch
+            {
+                var result = new Result()
+                {
+                    Error = "Invalid file settings.json",
+                    Primes = null,
+                    Success = false,
+                    Duration = null
+                };
+                LoadResult(result);
+                return null;
+            }
+
+            var listOfNum = JsonSerializer.Deserialize<List<Settings>>(jsonFile);
+
+            var noNullList = listOfNum.Select(x => x).Where(x =>x != null);
+
+            return noNullList.ToList();
+        }
+
+        public static void CreateResult(Stopwatch timer, List<int> listPrime)
+        {
+            var result = new Result
+            {
+                Success = true, Error = null, Duration = timer.Elapsed.ToString(), Primes = listPrime.ToArray()
+            };
+            LoadResult(result);
+
+        }
+
+        private static void LoadResult(Result resultOfArray)
+        {
+           
+                var result = JsonSerializer.Serialize(resultOfArray);
+                File.WriteAllText("Result.json", result);
+            
+        }
+    }
+}
